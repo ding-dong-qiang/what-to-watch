fd00eedc818faa55721d74a9587b46a8907c6237
Scott Yinan Fan <scottvan2008@gmail.com>
2024-11-24 10:28:07 +0800

    Update page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,15 @@
-export default function page() {
-  return (
-    <main>hello, world!</main>
-  );
+import Link from "next/link";
+
+export default function Home() {
+    return (
+        <main>
+            <h1 className="text-4xl">
+                What to watch
+            </h1>
+            <ul>
+                {/* <li><Link href="./week-2">Week 2 Assignment</Link></li> */}
+
+            </ul>
+        </main>
+    );
 }
